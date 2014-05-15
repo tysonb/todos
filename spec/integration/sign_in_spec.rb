2e6b44a0bf66a47dfd18ab035f921499c4ba8380
Tyson Brown <tysonb@buypd.com>
2014-05-15 16:39:16 -0600

    users can sign in with email address

---
+++ spec/integration/sign_in_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'Sign in as a user' do
+  scenario "with an email address" do
+# How are we going to be signing in
+# Not going to be just posting to a controller
+# or filling in a form
+# Its all these and more!
+    visit root_path
+    fill_in 'Email address', with: 'person@example.com'
+    click_button 'Sign In'
+    expect(page).to have_css '.welcome', text: 'Welcome, person@example.com'
+  end
+end
