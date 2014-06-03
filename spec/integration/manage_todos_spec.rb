0a649c3ec35210d0801561f7b07db98c71360259
Tyson Brown <tysonb@buypd.com>
2014-06-02 18:01:03 -0600

    30:00

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
-# We don't really want to interact with the database with our
-# integrations tests.  These are tests that are supposed to 
-# assert that a user can perform some interaction
-# and make sure they saw that interaction work successfully.
-# They're not going to be able to touch DB in a direct manner.
-# They will be looking for content on the page.
-
 feature 'Manage todos' do
   scenario 'create a new todo' do
     sign_in
@@ -16,14 +9,24 @@ feature 'Manage todos' do
   end
 
   scenario 'view only my todos' do
-    # replace the following line with Factory_Girl syntax
-#   Todo.create(description: 'Buy some eggs', owner_email: 'not_me@example.com')
     create(:todo, description: 'Buy some eggs', owner_email: 'not_me@example.com')
     sign_in_as "me@example.com" 
 
     create_todo_with_description 'Buy some milk'
+
     user_sees_todo_item 'Buy some milk'
     user_does_not_see_todo_item 'Buy some eggs'
+  end
+
+  scenario 'mark todos as complete' do
+    sign_in
+    create_todo_with_description 'Buy some milk'
+
+    within 'li.todo' do
+      click_link 'Complete'
+
+      expect(page).to have_css 'li.todo.completed'
+    end
   end
 
   def create_todo_with_description(description)
